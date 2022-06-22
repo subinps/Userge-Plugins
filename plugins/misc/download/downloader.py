0d2c17c9f65eb6ac1a3624e3d5cc9e08bb766cbf
alamia442 <61982500+alamia442@users.noreply.github.com>
2022-06-22 09:18:19 +0700

    Update downloader.py

--- plugins/misc/download/downloader.py
+++ plugins/misc/download/downloader.py
@@ -79,14 +79,14 @@ async def url_download(message: Message, url: str) -> Tuple[str, int]:
         url = url.strip()
         if c_file_name:
             custom_file_name = c_file_name.strip()
-    if "@" in url:
-        parsed = urlparse(url)
+    parsed = urlparse(url)
+    if parsed.username:
         usern = parsed.username
         passw = parsed.password
         url = parsed._replace(netloc=f"{parsed.hostname}").geturl()
     dl_loc = os.path.join(config.Dynamic.DOWN_PATH, custom_file_name)
     downloader = SmartDL(url, dl_loc, progress_bar=False)
-    downloader.add_basic_authentication(usern, passw)
+    downloader.add_basic_authentication(username=usern, password=passw)
     downloader.start(blocking=False)
     with message.cancel_callback(downloader.stop):
         while not downloader.isFinished():
